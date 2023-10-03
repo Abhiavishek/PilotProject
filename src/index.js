9f5c30b952313a3ceede66f9d9bbaf90816d3ae2
Abhiavishek <avi74500@gmail.com>
2023-10-03 19:20:06 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-// src/index.js (or your entry point)
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'; // Import the Provider component
+import React from "react";
+import {createRoot} from "react-dom/client";
+ import App from './App';
+ import { Provider } from 'react-redux'; 
 import store from './store'; // Import your Redux store
-import App from './App';
-
-ReactDOM.render(
-  // Wrap your App component with Provider and pass the store as a prop
+ createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
-);
+  </Provider>
+ )
+
